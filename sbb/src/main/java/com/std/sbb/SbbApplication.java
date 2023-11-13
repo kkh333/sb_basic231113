339097ff06feac25756da88995fc95e961b111f6
kkh <k2ho33333@gmail.com>
2023-11-13 10:47:15 +0900

    init

---
+++ sbb/src/main/java/com/std/sbb/SbbApplication.java
@@ -0,0 +1,14 @@
+package com.std.sbb;
+
+import lombok.Getter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbApplication.class, args);
+	}
+
+}
